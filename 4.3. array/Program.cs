79c69c8f898ebc0c3b7e0871e40ed4e485729c12
Yulia Miller <miller82.98@mail.ru>
2022-12-15 23:43:53 -0800

    Добавьте файлы проекта.

---
+++ 4.3. array/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace mynamespase
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Введите свое имя: ");
+            string name = Console.ReadLine();
+            
+            for(int i = name.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
